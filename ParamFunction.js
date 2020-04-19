12c01203dd91919c3fc1f22b9e491a8e5e2332b8
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-04-19 23:32:39 +0100

    Update ParamFunction.js

--- ParamFunction.js
+++ ParamFunction.js
@@ -1,4 +1,4 @@
-//Write a function that takes one parameter (a whole number/integer) and checks if numbers from 1 to the given parameter (inclusive) are divisible by 2,3,5 while replacing with the appropriate words as specified below
+//Write a function that takes one parameter (a whole number/integer) and checks if numbers from 1 to the given parameter (inclusive) are divisible by 2,3,5.
 
 
 function paramDiv(paramtr) {
